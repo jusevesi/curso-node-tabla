ed2e5c45858179e7020d2082c9a1f81bc327147f
Juan Vera <juanvera1795@gmail.com>
2021-10-06 17:03:42 -0500

    Primer commit

---
+++ helpers/multiplicar.js
@@ -0,0 +1,37 @@
+const fs= require('fs');
+const colors = require('colors');
+
+const crearArchivo = async( base = 5 , list = false, hasta =10) =>{
+    try {
+        
+
+        let salida, consola = '';
+        salida = `=============\n Tabla del ${base}\n=============\n`; 
+
+        for(let i = 1; i<=hasta; i++){
+            //interpolacion de strings "ventajas de esto"
+            consola += (`${base} ${'x'.red} ${i} ${'='.red}' ${base*i}\n`);
+            salida += (`${base} ${'x'} ${i} ${'='}' ${base*i}\n`);
+        }
+        if(list==true){
+            console.log('=============='.red);
+            console.log(' Tabla del:', colors.blue(base) );
+            console.log('=============='.red);
+
+            console.log(consola);                    
+        }
+        //si sucede error hay que usar try-catch
+    
+        fs.writeFileSync(`tabla-${base}.txt`, salida);
+        return `tabla-${base}.txt`;
+        
+    } catch (error) {
+        throw(err);
+    }
+     
+}
+
+//module es un objeto global que existe en node
+module.exports = {
+    crearArchivo
+}
